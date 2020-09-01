8d94014bd24644db9fd8cdae5ac8970ba74bff49
Eneko <enekouru@gmail.com>
2020-09-01 13:03:28 +0200

    More testing on notebook

--- low_rank_pfm/low_rank_pfm.py
+++ low_rank_pfm/low_rank_pfm.py
@@ -1,13 +1,16 @@
 import sys
 
+import nibabel as nib
 import numpy as np
 import scipy as sci
 from nilearn.input_data import NiftiMasker
+from nilearn.masking import apply_mask, unmask
 from nilearn.image import load_img, new_img_like
 
 from low_rank_pfm.cli.run import _get_parser
 from low_rank_pfm.src.low_rank import low_rank
 from low_rank_pfm.src.hrf_matrix import HRFMatrix
+from low_rank_pfm.io import new_nii_like
 
 
 def debiasing(x, y, beta):
@@ -15,12 +18,12 @@ def debiasing(x, y, beta):
     beta_out = np.zeros(beta.shape)
     fitts_out = np.zeros(y.shape)
 
-    index_voxels = np.unique(np.where(abs(beta) > 1e-3)[1])
+    index_voxels = np.unique(np.where(abs(beta) > 1e-2)[1])
 
     print('Debiasing results...')
     for voxidx in range(len(index_voxels)):
         index_events_opt = np.where(
-            abs(beta[:, index_voxels[voxidx]]) > 1e-3)[0]
+            abs(beta[:, index_voxels[voxidx]]) > 1e-2)[0]
 
         if index_events_opt.size != 0:
             X_events = x[:, index_events_opt]
@@ -42,9 +45,13 @@ def debiasing(x, y, beta):
 
 def low_rank_pfm(data_filename, mask_filename, output_filename, tr, te=[0]):
 
-    # data_img = load_img(data_filename, dtype='float32')jupy
-    masker = NiftiMasker(mask_img=mask_filename, standardize=False)
-    data_masked = masker.fit_transform(data_filename)
+    data_img = load_img(data_filename, dtype='float32')
+    mask_img = load_img(mask_filename, dtype='float32')
+    data_masked = apply_mask(data_img, mask_img)
+    # breakpoint()
+    # masker = NiftiMasker(mask_img=mask_filename, standardize=False)
+    # data_masked = masker.fit_transform(data_filename)
+    # masker.t_r = tr
 
     hrf_obj = HRFMatrix(TR=tr, nscans=int(data_masked.shape[0]), TE=te)
     hrf_norm = hrf_obj.generate_hrf().X_hrf_norm
@@ -54,20 +61,31 @@ def low_rank_pfm(data_filename, mask_filename, output_filename, tr, te=[0]):
     # Debiasing
     S_deb, S_fitts = debiasing(x=hrf_norm, y=data_masked, beta=S)
 
-    masker.inverse_transform(L).to_filename(f'{output_filename}_fluc.nii.gz')
+    breakpoint()
+
+    # masker.inverse_transform(L).to_filename(f'{output_filename}_fluc.nii.gz')
+    L_img = unmask(L, mask_img)
+    L_img.header.from_header(header=data_img.header)
     # nii = new_img_like(ref_niimg=data_img, data=L_img, copy_header=True)
-    # nii.set_data_dtype(L_img.dtype)
-    # nii.to_filename('L.nii.gz')
+    # L_nib = nib.Nifti1Image(L_img, affine=data_img.affine, header=data_img.header)
+    # L_nii = new_nii_like(ref_img=data_img, data=L, affine=data_img.affine)
+    L_img.to_filename(f'{output_filename}_fluc.nii.gz')
 
-    masker.inverse_transform(S_deb).to_filename(f'{output_filename}_beta.nii.gz')
+    # masker.inverse_transform(S_deb).to_filename(f'{output_filename}_beta.nii.gz')
+    S_img = unmask(S_deb, mask_img)
+    S_img.header.from_header(header=data_img.header)
     # nii = new_img_like(ref_niimg=data_img, data=S_img, copy_header=True)
-    # nii.set_data_dtype(S_img.dtype)
-    # nii.to_filename('S.nii.gz')
+    # S_nib = nib.Nifti1Image(S_img, affine=data_img.affine, header=data_img.header)
+    # S_nii = new_nii_like(ref_img=data_img, data=S_deb, affine=data_img.affine)
+    S_img.to_filename(f'{output_filename}_beta.nii.gz')
 
-    masker.inverse_transform(S_fitts).to_filename(f'{output_filename}_fitts.nii.gz')
+    # masker.inverse_transform(S_fitts).to_filename(f'{output_filename}_fitts.nii.gz')
+    S_fitts_img = unmask(S_fitts, mask_img)
+    S_fitts_img.header.from_header(header=data_img.header)
     # nii = new_img_like(ref_niimg=data_img, data=S_fitts_img, copy_header=True)
-    # nii.set_data_dtype(S_fitts_img.dtype)
-    # nii.to_filename('fitts.nii.gz')
+    # S_fitts_nib = nib.Nifti1Image(S_fitts_img, affine=data_img.affine, header=data_img.header)
+    # S_fitts_nii = new_nii_like(ref_img=data_img, data=S_fitts, affine=data_img.affine)
+    S_fitts_img.to_filename(f'{output_filename}_fitts.nii.gz')
 
 
 def _main(argv=None):
--- low_rank_pfm/src/hrf_matrix.py
+++ low_rank_pfm/src/hrf_matrix.py
@@ -37,17 +37,17 @@ def hrf_linear(RT,p):
     b1 = 1 / p[3]
     a2 = p[1] / p[3] 
     b2 = 1 / p[3]
-
+    
     hrf = (scipy.stats.gamma.pdf(u*dt, a1, scale = b1) - scipy.stats.gamma.pdf(u*dt, a2, scale = b2)/p[4])/dt
     time_axis = np.arange(0, int(p[6]/RT+1), 1) * fMRI_T
     hrf = hrf[time_axis]
     min_hrf = 1e-9*min(hrf[hrf > 10*np.finfo(float).eps])
-
+    
     if min_hrf < 10*np.finfo(float).eps:
         min_hrf = 10*np.finfo(float).eps
-
+    
     hrf[hrf == 0] = min_hrf
-
+    
     return(hrf)
 
 def hrf_afni(tr, lop_hrf):
@@ -56,30 +56,31 @@ def hrf_afni(tr, lop_hrf):
     # Increases duration until last HRF sample is zero
     while last_hrf_sample != 0:
         dur_hrf = 2*dur_hrf
-        npoints_hrf = np.round(dur_hrf, int(tr))
+        npoints_hrf = np.round(dur_hrf, tr)    
         hrf_command = '3dDeconvolve -x1D_stop -nodata %d %f -polort -1 -num_stimts 1 -stim_times 1 \'1D:0\' \'%s\' -quiet -x1D stdout: | 1deval -a stdin: -expr \'a\'' %(dur_hrf, tr, lop_hrf)
         hrf_tr_str = subprocess.check_output(hrf_command, shell = True, universal_newlines = True).splitlines()
         hrf_tr = np.array([float(i) for i in hrf_tr_str])
         last_hrf_sample = hrf_tr[len(hrf_tr)-1]
         if last_hrf_sample != 0:
             print('Duration of HRF was not sufficient for specified model. Doubling duration and computing again.')
-
+        
     # Removes tail of zero samples
     while last_hrf_sample == 0:
         hrf_tr = hrf_tr[0:len(hrf_tr)-1]
         last_hrf_sample = hrf_tr[len(hrf_tr)-1]
-
+    
     return(hrf_tr)
 
 class HRFMatrix:
 
-    def __init__(self, TR=2.0, TE=None, nscans=200, r2only=1, is_afni=True, lop_hrf='SPMG1'):
+    def __init__(self, TR=2, TE=None, nscans=200, r2only=1, is_afni=True, lop_hrf='SPMG1', has_integrator=True):
         self.TR = TR
         self.TE = TE
         self.nscans = nscans
         self.r2only = r2only
         self.lop_hrf = lop_hrf
         self.is_afni = is_afni
+        self.has_integrator = has_integrator
 
     def generate_hrf(self):
 
@@ -88,14 +89,14 @@ class HRFMatrix:
         else:
             p = [6,16,1,1,6,0,32]
             hrf_SPM = hrf_linear(self.TR, p)
-
+  
         self.L_hrf = len(hrf_SPM) # Length
         max_hrf = max(abs(hrf_SPM)) # Max value
         filler = np.zeros(self.nscans-hrf_SPM.shape[0], dtype=np.int)
         hrf_SPM = np.append(hrf_SPM, filler) # Fill up array with zeros until nscans
-
+    
         temp = hrf_SPM
-
+        
         for i in range(self.nscans-1):
             foo = np.append(np.zeros(i+1), hrf_SPM[0:(len(hrf_SPM)-i-1)])      
             temp = np.column_stack((temp, foo))
@@ -106,12 +107,21 @@ class HRFMatrix:
                 tempTE = np.vstack((tempTE, -self.TE[teidx+1]*temp))
         else:
             tempTE = temp
-
+        
         if self.r2only:
             self.X_hrf = tempTE
 
-        #  self.X_hrf = np.matmul(X_hrf, np.tril(np.ones(self.nscans)))
+        self.X_hrf_norm = self.X_hrf/max_hrf
 
-        self.X_hrf_norm = self.X_hrf / max_hrf
+        if self.has_integrator:
+            if len(self.TE) > 1:
+                for teidx in range(len(self.TE)):
+                    temp = self.X_hrf[teidx*self.nscans:(teidx+1)*self.nscans-1,:].copy()
+                    self.X_hrf[teidx*self.nscans:(teidx+1)*self.nscans-1,:] = np.matmul(temp, np.tril(np.ones(self.nscans)))
+                    temp = self.X_hrf_norm[teidx*self.nscans:(teidx+1)*self.nscans-1,:].copy()
+                    self.X_hrf_norm[teidx*self.nscans:(teidx+1)*self.nscans-1,:] = np.matmul(temp, np.tril(np.ones(self.nscans)))
+            else:
+                self.X_hrf = np.matmul(self.X_hrf, np.tril(np.ones(self.nscans)))
+                self.X_hrf_norm = np.matmul(self.X_hrf_norm, np.tril(np.ones(self.nscans)))
 
         return(self)
