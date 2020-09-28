e8c07a77a294f12eaa3a919b58217ccf356af0c7
Eneko <enekouru@gmail.com>
2020-09-28 18:11:42 +0200

    Bug fix

--- low_rank_pfm/src/low_rank.py
+++ low_rank_pfm/src/low_rank.py
@@ -380,6 +380,6 @@ def low_rank(data, hrf, maxiter=1000, miniter=10, vox_2_keep=0.3, nruns=1, lambd
     Ut, St, Vt = svd(np.nan_to_num(l_final), full_matrices=False, compute_uv=True,
                      check_finite=True)
     eig_vecs = Ut[:, :keep_idx]
-    eig_maps = Ut[:keep_idx, :]
+    eig_maps = Vt[:keep_idx, :]
 
     return(l_final, S, eig_vecs, eig_maps)
