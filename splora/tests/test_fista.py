5cdf2f417adf1652461f4e7def53f60fb6f5e399
Eneko Uruñuela <e.urunuela@icloud.com>
2023-11-20 18:12:31 +0100

    Fixed tests after MAD values changed

--- splora/tests/test_fista.py
+++ splora/tests/test_fista.py
@@ -29,37 +29,38 @@ def test_proximal_operator_mixed_norm():
 
 
 def test_select_lambda():
+    noise_expected = 0.0015168392317151877
     # MAD Update and MAD
     lambda_selec, update_lambda, noise = fista.select_lambda(hrf=hrf, y=y, criteria="mad_update")
-    assert np.allclose(lambda_selec, 0.002248865844940937)
+    assert np.allclose(lambda_selec, noise_expected)
     assert update_lambda
-    assert np.allclose(noise, 0.002248865844940937)
+    assert np.allclose(noise, noise_expected)
 
     # Universal threshold
     lambda_selec, update_lambda, noise = fista.select_lambda(hrf=hrf, y=y, criteria="ut")
-    assert np.allclose(lambda_selec, 0.004721675779877547)
+    assert np.allclose(lambda_selec, 0.0031847266827718513)
     assert not update_lambda
-    assert np.allclose(noise, 0.002248865844940937)
+    assert np.allclose(noise, noise_expected)
 
     # Lower universal threshold
     lambda_selec, update_lambda, noise = fista.select_lambda(hrf=hrf, y=y, criteria="lut")
-    assert np.allclose(lambda_selec, 0.0041566221123978085)
+    assert np.allclose(lambda_selec, 0.002803603205448407)
     assert not update_lambda
-    assert np.allclose(noise, 0.002248865844940937)
+    assert np.allclose(noise, noise_expected)
 
     # Factor
     lambda_selec, update_lambda, noise = fista.select_lambda(
         hrf=hrf, y=y, criteria="factor", factor=10
     )
-    assert np.allclose(lambda_selec, 0.022488658449409372)
+    assert np.allclose(lambda_selec, 0.015168392317151877)
     assert not update_lambda
-    assert np.allclose(noise, 0.002248865844940937)
+    assert np.allclose(noise, noise_expected)
 
     # Percentage
     lambda_selec, update_lambda, noise = fista.select_lambda(hrf=hrf, y=y, criteria="pcg", pcg=0.8)
     assert np.allclose(lambda_selec, 0.07034059283844483)
     assert not update_lambda
-    assert np.allclose(noise, 0.002248865844940937)
+    assert np.allclose(noise, noise_expected)
 
 
 def test_fista():
@@ -72,5 +73,5 @@ def test_fista():
         factor=20,
     )
     assert np.allclose(beta, y_out, atol=1e-5)
-    assert noise_est[0] == 0.002248865844940937
-    assert lambda_val[0] == 0.044977316898818745
+    assert noise_est[0] == 0.0015168392317151877
+    assert lambda_val[0] == 0.030336784634303754
